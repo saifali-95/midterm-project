eb670e86be79cb0aaa442a4da98d61898a9f8b72
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-08-24 01:21:51 +0000

    add favourite route for both GET and POST requestto add favourite items or/and see them

---
+++ routes/favourite.js
@@ -0,0 +1,46 @@
+/*
+ * All routes for Login are defined here
+ * Since this file is loaded in server.js into api/login,
+ *   these routes are mounted onto /login
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+
+  router.get("/", (req, res) => {
+    const {user_id} = req.session.id;
+    db.query(`
+      SELECT * FROM favourites
+      JOIN products ON product_id = products.id
+      WHERE id = $1
+    `, user_id)
+    .then(data => {
+      templateVars = {
+        products: data.rows
+      }
+    })
+    res.render("favourite", products);
+  });
+  router.post("/", (req, res) => {
+    const {product_id} = req.body;
+    if (req.session.user_id) {
+      const user_id = req.session.user_id;
+      db.query(`
+        INSERT INTO favourites (user_id, product_id)
+        VALUES($1, $2);
+        `, [user_id, product_id])
+        .then()
+        .catch(err => {
+          console.log("catching an error", err);
+          res.send().status().json({err: err.message})
+        }) ;
+         return res.redirect("/");
+    }
+      return res.send("Please login first!!!");
+  });
+
+  return router;
+};
--- server.js
+++ server.js
@@ -40,11 +40,14 @@ const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
 const loginRoutes = require("./routes/login");
 const logoutRoutes = require("./routes/logout");
+const favouriteRoutes = require("./routes/favourite");
+
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/logout", logoutRoutes());
 app.use("/login", loginRoutes(db));
+app.use("/favourite", favouriteRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
