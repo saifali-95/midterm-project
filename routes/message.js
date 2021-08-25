0bb48e560ddc5bacefd5bc24750f4acaa3211d26
Saif Ali <saifali7646@gmail.com>
2021-08-25 03:36:15 +0000

    add a contact seller option to intiate a chat from buyer to seller

--- routes/message.js
+++ routes/message.js
@@ -10,31 +10,42 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
 const client = require('twilio')(accountSid, authToken);
 const express = require('express');
+const { LogInstance } = require('twilio/lib/rest/serverless/v1/service/environment/log');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    res.render("message");
+  router.get("/:id", (req, res) => {
+    const product_id = req.params.id;
+    const templateVars = {product_id};
+    res.render("message", templateVars);
   });
-  router.post("/", (req, res) => {
-    const seller_id = 2;
+
+  router.post("/:id", (req, res) => {
+
     const message = req.body.message;
+    const product_id = req.params.id;
+    const buyer = req.session.user_id;
 
     db.query(`
-    SELECT * FROM users
-    WHERE id = $1;
-    `,[seller_id])
+    SELECT phone, users.id FROM users JOIN products ON seller_id = users.id
+    WHERE products.id = $1;
+    `,[product_id])
+
     .then(data => {
+      const seller_id = data.rows[0].id;
+      const phone =  data.rows[0].phone;
+
       db.query(`INSERT INTO chats (from_id, to_id, message, product_id)
       VALUES ($1, $2, $3, $4)
-      `, [1, 2, `${message}`, 2])
+      `, [buyer, seller_id , `${message}`, product_id])
 
       const user = data.rows[0];
+      console.log('user phone:', user);
       client.messages
       .create({
         body: `${message}`,
         from: `${twilioNumber}`,
-        to: `${user.phone}`
+        to: `${phone}`
       })
       return;
     })
