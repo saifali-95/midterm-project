890b2e4b3f53166774bbca2c5e4dcd097a82a99d
Saif Ali <saifali7646@gmail.com>
2021-08-25 17:04:54 +0000

    database modified to initiate chat_service_id and store all conversation according to it

--- db/seeds/06_chats.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO chats (from_id, to_id, message, product_id)
-VALUES (1, 2, 'I want to buy this product', 2);
-
-INSERT INTO chats (from_id, to_id, message, product_id)
-VALUES (2, 1, 'Sorry the item is sold', 2);
